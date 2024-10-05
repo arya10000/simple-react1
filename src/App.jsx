54c5a32aea217fccf84c3aa93229c3f1de21a4e3
arya10000 <aryamaitys@gmail.com>
2024-10-05 14:01:38 +0530

    First comment

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Team from './components/Team';
+import Faq from './components/Faq';
+import Cta from './components/Cta';
+import About from './components/About';
+import Available from './components/Available';
+import Features from './components/Features';
+import Header from './components/Header'
+import Hero from './components/Hero'
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Features />
+      <Available />
+      <About />
+      <Cta />
+      <Faq />
+      <Team />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
